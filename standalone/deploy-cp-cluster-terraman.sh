dc5e8cd4f54c2566b0a2943b3714128767ed3a37
Taewoo Kim <67407365+dev-taewoo@users.noreply.github.com>
2023-11-20 10:52:14 +0900

    Modify terraman script

--- standalone/deploy-cp-cluster-terraman.sh
+++ standalone/deploy-cp-cluster-terraman.sh
@@ -10,6 +10,8 @@ cp roles/kubernetes-apps/metrics_server/defaults/main.yml.ori roles/kubernetes-a
 sed -i "s/{MASTER1_NODE_HOSTNAME}/$MASTER1_NODE_HOSTNAME/g" roles/kubernetes-apps/metrics_server/defaults/main.yml
 sed -i "s/{MASTER1_NODE_PUBLIC_IP}/$MASTER1_NODE_PUBLIC_IP/g" roles/kubernetes/control-plane/tasks/kubeadm-setup.yml
 
+sed -i "s/metallb_enabled: true/metallb_enabled: false/g" inventory/mycluster/group_vars/k8s_cluster/addons.yml
+
 cp $HOME/.ssh/$CLUSTER_PRIVATE_KEY $HOME/.ssh/id_rsa
 cp $HOME/.ssh/$CLUSTER_PRIVATE_KEY.pub $HOME/.ssh/id_rsa.pub
 
